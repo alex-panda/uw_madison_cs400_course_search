ae96cd7338f8f0ec8b4288550e82446b8ce66759
Ben Wurster <55667072+BWurster@users.noreply.github.com>
2020-09-28 14:59:06 -0500

    Version, fix temp

---
+++ Version/Ben/DataLoader.java
@@ -0,0 +1,118 @@
+// --== CS400 File Header Information ==--
+// Name: Xiaohan Shen
+// Email: xshen97@wisc.edu
+// Team: DC
+// TA: Yelun
+// Lecturer: Gary Dahl
+// Notes to Grader: None
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Loads course information including courses' prerequisites and courses that cannot be taken prior to taking certain
+ * courses from target csv file.
+ *
+ * @author Xiaohan Shen
+ */
+public class DataLoader{
+    private final String csvPath;
+
+    /**
+     * Constructs a DataLoader object that reads course data from csv file at specified path.
+     * This constructor is intended for testing purposes since one can choose to load from test csv file.
+     *
+     * @param csvPath is the path of the target csv file
+     */
+    public DataLoader(String csvPath) {
+        this.csvPath = csvPath;
+    }
+
+    /**
+     * Constructs a DataLoader that reads course data from default csv file.
+     */
+    public DataLoader() {
+        csvPath = "CS_course_info.csv";
+    }
+
+    /**
+     * Returns the course data read from target csv file.
+     *
+     * @return an ArrayList of Course objects that includes all course data from target csv file.
+     */
+    public ArrayList<Course> getData() {
+        ArrayList<String[]> courseList = readData();
+        ArrayList<Course> courses = new ArrayList<>();
+        for (String[] courseInfo: courseList) {
+            String courseName = courseInfo[0];
+            ArrayList<Object> prereqs = (courseInfo[1].equals("None")) ? null : parsePrereqs(courseInfo[1]);
+            ArrayList<String> noreqs = new ArrayList<>();
+            if ((courseInfo[2].equals("None"))) {
+                noreqs = null;
+            } else {
+                noreqs.addAll(Arrays.asList(courseInfo[2].split("&")));
+            }
+            courses.add(new Course(courseName, prereqs, noreqs));
+        }
+        return courses;
+    }
+
+    /**
+     * Helper method that parses the prerequisites stored in csv file.
+     *
+     * @param prereqs is the String of prerequisites in csv file.
+     * @return the prerequisites represented in prereqs.
+     */
+    private ArrayList<Object> parsePrereqs(String prereqs) {
+        ArrayList<Object> parsedPrereqs = new ArrayList<>();
+        String[] requiredParts = prereqs.split("&");
+        for (String part: requiredParts){
+            if (part.contains("|")){
+                part = part.replaceAll("[()]","");
+                String[] alternatives = part.split("\\|");
+                parsedPrereqs.add(Arrays.asList(alternatives));
+            } else {
+                parsedPrereqs.add(part);
+            }
+        }
+        return parsedPrereqs;
+    }
+
+    /**
+     * Helper method that reads data from csv file at csvPath
+     *
+     * @return an ArrayList of String arrays containing each course's course number, prerequisites,
+     * and courses that cannot be taken prior to the course.
+     */
+    private ArrayList<String[]> readData() {
+        BufferedReader csvReader = null;
+        try {
+            csvReader = new BufferedReader(new FileReader(csvPath));
+            String line = "";
+            ArrayList<String[]> courseList = new ArrayList<>();
+            csvReader.readLine();
+            while ((line = csvReader.readLine()) != null) {
+                // use comma as separator
+                String[] courseInfo = line.split(",");
+                courseList.add(courseInfo);
+            }
+            return courseList;
+        } catch (FileNotFoundException e) {
+            System.out.println("Course information file " + csvPath + " was not found!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert csvReader != null;
+                csvReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
---
+++ Version/Ben/PairNode.java
@@ -0,0 +1,34 @@
+// --== CS400 File Header Information ==--
+// Name: Benjamin Ryan Wurster
+// Email: bwurster@wisc.edu
+// Team: DC
+// TA: Yelun
+// Lecturer: Gary Dahl
+// Notes to Grader: None
+
+/**
+ * Object that pairs the KeyType generic and the ValueType generic into one
+ * object.
+ * 
+ * @author Ben
+ *
+ * @param <KeyType>   generic type for key
+ * @param <ValueType> generic type for returned value
+ */
+public class PairNode<KeyType, ValueType> {
+	private KeyType key;
+	private ValueType value;
+
+	public PairNode(KeyType key, ValueType value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public KeyType getKey() {
+		return key;
+	}
+
+	public ValueType getValue() {
+		return value;
+	}
+}
